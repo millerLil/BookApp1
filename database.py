1dfc810dc97baf127c5b7d776838a020b7882a3a
millerLil <91765948+millerLil@users.noreply.github.com>
2025-03-16 16:34:20 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,40 +2,40 @@ import sqlite3
 import userStore
 
 def get_db_connection():
-    conn = sqlite3.connect('userDB.db', timeout=10.0)
+    conn = sqlite3.connect('database.db', timeout=10.0)
     conn.row_factory = sqlite3.Row
     return conn
 
-def update_currentBook(userName, currentBook):
+def update_Weight(userName, weight):
     conn = get_db_connection()
     cur = conn.cursor()
     
     try:
-        res = cur.execute("UPDATE users SET userCurrentBook = ? WHERE userName = ?", (currentBook, userName))
-        userStore.set_currentBook(currentBook)
-        message = "The current book you are reading is successfully updated"
+        res = cur.execute("UPDATE users SET userWeight = ? WHERE userName = ?", (weight, userName))
+        userStore.set_weight(weight)
+        message = "Weight successfully updated"
         print(message)
     except sqlite3.Error as e:
-        print(f"Database error: {e}") 
+        print(f"Database error: {e}")  # Prints the full error message
 
     conn.commit()
     cur.close()
     conn.close()   
     print("connection cleaned")
 
-def get_CurrentBook_from_db(userName):
+def get_Weight_from_db(userName):
     conn = get_db_connection()
     cur = conn.cursor()
-    currentBook = ''
+    weight = 0
 
     try:
-        res = cur.execute("SELECT userCurrentBook FROM users WHERE userName = ?", (userName, ))
+        res = cur.execute("SELECT userWeight FROM users WHERE userName = ?", (userName, ))
         result = cur.fetchone()
         weight = result[0]
-        message = "Book you are reading is successfully retrieved"
+        message = "Weight successfully retrieved"
         print(message)
     except sqlite3.Error as e:
         print(f"Database error: {e}")  # Prints the full error message
 
     conn.close()
-    return currentBook
+    return weight
